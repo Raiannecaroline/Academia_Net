bffcbf5fb1d52c864105997a3efb8b291ff8d549
Raiane Caroline S Teixeira <raianecaroline.st@gmail.com>
2024-06-13 20:42:19 -0300

    Update: Modificação das Classes e Criação ex 04

---
+++ Aula02/Exercicio04/Triangulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academia_Net.Aula02.Exercicio04
+{
+    public class Triangulo : FormaGeometrica
+    {
+
+        public Triangulo(double largura, double altura) : base(largura, altura) { }
+
+        public override double CalcularArea()
+        {
+            return (Largura * Altura) / 2;
+        }
+    }
+}
