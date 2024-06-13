2d293a8b469f1f1680170392a01bc97fd09a42e3
Raiane Caroline S Teixeira <raianecaroline.st@gmail.com>
2024-06-13 20:42:19 -0300

    Update: Modificação das Classes e Criação ex 04

---
+++ Aula02/Exercicio04/Circulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academia_Net.Aula02.Exercicio04
+{
+    public class Circulo : FormaGeometrica
+    {
+        public double Raio {  get; set; }
+
+        public Circulo(double raio) : base(0, 0) 
+        {
+            Raio = raio;
+        }
+
+        public override double CalcularArea()
+        {
+            return Math.PI * Raio * Raio;
+        }
+    }
+}
--- Aula02/MainProjects.cs
+++ Aula02/MainProjects.cs
@@ -1,4 +1,5 @@
-﻿using Academia_Net.Exercício01;
+﻿using Academia_Net.Aula02.Exercicio04;
+using Academia_Net.Exercício01;
 using Academia_Net.Exercicio02;
 using System;
 using System.Collections.Generic;
@@ -30,31 +31,48 @@ namespace Academia_Net
             ((Cachorro) cachorro).AbanarRabo();
             ((Gato)gato).Rononar();*/
 
-            /*Exercício 02 abaixo:*/
+            /*Exercício 02 e 03 abaixo:*/
 
             // Referência de Objeto da Conta Corrente
-            ContaCorrente contaCorrente = new ContaCorrente("Raiane Caroline", 5000m);
+            /*ContaCorrente contaCorrente = new ContaCorrente("Raiane Caroline", 5000m);
             contaCorrente.Depositar(1000m);
             contaCorrente.Sacar(1000m);
             contaCorrente.ConsultarSaldo();
 
-            Console.WriteLine();
+            Console.WriteLine();*/
 
             // Referência de Objeto da Conta Poupança
-            ContaPoupanca contaPoupanca = new ContaPoupanca("Raiane Caroline", 10000m);
+            /*ContaPoupanca contaPoupanca = new ContaPoupanca("Raiane Caroline", 10000m);
             contaPoupanca.Depositar(10000m);
             contaPoupanca.AplicacaoInvestimento();
             contaPoupanca.Sacar(5000m);
             contaPoupanca.ConsultarSaldo();
 
-            Console.WriteLine();
+            Console.WriteLine();*/
 
             // Referência de Objeto da Conta de Investimento
-            ContaInvestimento contaInvestimento = new ContaInvestimento("Raiane Caroline", 5000m);
-            contaInvestimento.Depositar(2000m);
-            contaInvestimento.AplicacaoInvestimento();
-            contaInvestimento.Sacar(1000m);
-            contaInvestimento.ConsultarSaldo();
+            /* ContaInvestimento contaInvestimento = new ContaInvestimento("Raiane Caroline", 5000m);
+             contaInvestimento.Depositar(2000m);
+             contaInvestimento.AplicacaoInvestimento();
+             contaInvestimento.Sacar(1000m);
+             contaInvestimento.ConsultarSaldo();*/
+
+            /*Exercício 04*/
+
+            // A Área do Triângulo
+            Triangulo triangulo = new Triangulo(5, 10);
+            double areaTriangulo = triangulo.CalcularArea();
+            Console.WriteLine("Área do Triângulo: " + areaTriangulo);
+
+            // A Área do Círculo
+            Circulo circulo = new Circulo(7);
+            double areaCirculo = circulo.CalcularArea();
+            Console.WriteLine("Área do Círculo: " + areaCirculo);
+
+            // A Área do Retângulo
+            Retangulo retangulo = new Retangulo(4, 8);
+            double areaRetangulo = retangulo.CalcularArea();
+            Console.WriteLine("Área do Retângulo: " + areaRetangulo);
         }
     }
 }
