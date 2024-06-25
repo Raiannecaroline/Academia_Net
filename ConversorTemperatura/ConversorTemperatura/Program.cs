de50562b81999c991db05022693fad52ce2e1b5f
Raiane Caroline S Teixeira <raianecaroline.st@gmail.com>
2024-06-24 21:12:07 -0300

    update: Atividades C#

---
+++ ConversorTemperatura/ConversorTemperatura/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorTemperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"Qual a temperatura registrada hoje? \n");
+            int temperaturaConvertida = Convert.ToInt32( Console.ReadLine() );
+
+
+            Console.WriteLine("Deseja ver a temperatura em °C (Opção 1) ou °F (Opção 2): \n");
+            int opcaoEscolhida = Convert.ToInt32( Console.ReadLine());
+
+            switch ( opcaoEscolhida )
+            {
+                case 1:
+                    int temperaturaCelcius = (temperaturaConvertida - 32) * 5 / 9;
+                    Console.WriteLine($"A temperatura em Celsius é de {temperaturaCelcius}°C\n");
+                    break;
+                case 2:
+                    int temperaturaFahrenheit = (temperaturaConvertida * 9 / 5) + 32;
+                    Console.WriteLine($"A temperatura em Fahrenheit é de {temperaturaFahrenheit}°F");
+                    break;
+
+                default:
+                    Console.WriteLine("Nenhuma opção escolhida");
+                    break;
+            }
+        }
+    }
+}
---
+++ MediaAluno/MediaAluno/Execicio01/CalculadoraMediaAlunos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaAluno
+{
+    internal class CalculadoraMediaAlunos
+    {
+        static void Main(string[] args)
+        {
+            decimal notasSoma = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"\nDigite a {i + 1}° nota do Aluno: \n");
+                decimal notas = Convert.ToDecimal(Console.ReadLine());
+
+                notasSoma += notas;
+            }
+
+            decimal mediaAluno = (notasSoma / 4) / 10;
+
+            if (mediaAluno >= 7)
+            {
+                Console.WriteLine($"\nO Aluno foi aprovado com a média de {mediaAluno:F1} \n");
+            }
+            else if (mediaAluno >= 5 && mediaAluno < 7)
+            {
+                Console.WriteLine($"\nO Aluno ficou com a média de {mediaAluno:F1} e está em Recuperação!!\n");
+            }
+            else
+            {
+                Console.WriteLine($"\nO Aluno foi reprovado com a média de {mediaAluno:F1}");
+            }
+
+        }
+    }
+}
